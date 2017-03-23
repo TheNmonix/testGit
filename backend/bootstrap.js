fe0553b821684337fde8203505b3409d7a8a831f
Najib <najib@iMac-de-Najib.local>
2017-03-23 16:09:44 +0000

    ^^^^^

--- backend/bootstrap.js
+++ backend/bootstrap.js
@@ -1,1 +1,1 @@
-/* * A BootStrap is a JavaScript file that is run when your project is loaded on the server. * You can use it  to initialize your application, define HTTP pattern handlers etc.. */   sd sd  d ScreenEventMapds
+/* * A BootStrap is a JavaScript file that is run when your project is loaded on the server. * You can use it  to initialize your application, define HTTP pattern handlers etc.. */   sd sd  d ScreenEventMapds  dd
